2c0500c3dc107ac93122900cfc090a5e16a88de0
Felix Habert <felix.habert.20@ucl.ac.uk>
2024-04-25 13:18:28 +0200

    added only square

--- classical_gan.py
+++ classical_gan.py
@@ -11,6 +11,7 @@ from torchvision import transforms
 import pandas as pd
 import torch.optim as optim
 import torchvision.utils as vutils
+import os 
 
 N = 3000
 IMAGE_SIZE = 28
@@ -44,6 +45,8 @@ def create_csv():
             flattened_row = row.flatten()
             writer.writerow(flattened_row)
     pass
+
+create_csv()
 
 class Discriminator(nn.Module):
     def __init__(self,ngpu):
@@ -148,54 +151,54 @@ G_losses = []
 D_losses = []
 iters = 0
 
-print("Starting Training Loop...")
-for epoch in range(num_epochs):
-    for i, data in enumerate(dataloader, 0):
-        # Real images
-        netD.zero_grad()
-        real_cpu = data[0].to(device)
-        real_cpu = real_cpu.unsqueeze(0)
-        b_size = real_cpu.size(0)
-        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
-        real_cpu = real_cpu.unsqueeze(0)
-        output = netD(real_cpu).view(-1)
-        print(output)
-        errD_real = criterion(output, label)
-        errD_real.backward()
-        D_x = output.mean().item()
-        # Fake images
-        noise = torch.randn(b_size, nz, 1, 1, device=device)
-        fake = netG(noise)
-        label.fill_(fake_label)
-        output = netD(fake.detach()).view(-1)
-        errD_fake = criterion(output, label)
-        errD_fake.backward()
-        D_G_z1 = output.mean().item()
-        errD = errD_real + errD_fake
-        optimizerD.step()
+# print("Starting Training Loop...")
+# for epoch in range(num_epochs):
+#     for i, data in enumerate(dataloader, 0):
+#         # Real images
+#         netD.zero_grad()
+#         real_cpu = data[0].to(device)
+#         real_cpu = real_cpu.unsqueeze(0)
+#         b_size = real_cpu.size(0)
+#         label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+#         real_cpu = real_cpu.unsqueeze(0)
+#         output = netD(real_cpu).view(-1)
+#         print(output)
+#         errD_real = criterion(output, label)
+#         errD_real.backward()
+#         D_x = output.mean().item()
+#         # Fake images
+#         noise = torch.randn(b_size, nz, 1, 1, device=device)
+#         fake = netG(noise)
+#         label.fill_(fake_label)
+#         output = netD(fake.detach()).view(-1)
+#         errD_fake = criterion(output, label)
+#         errD_fake.backward()
+#         D_G_z1 = output.mean().item()
+#         errD = errD_real + errD_fake
+#         optimizerD.step()
 
-        netG.zero_grad()
-        label.fill_(real_label)
-        output = netD(fake).view(-1)
-        errG = criterion(output, label)
-        errG.backward()
-        D_G_z2 = output.mean().item()
-        optimizerG.step()
-        if i % 50 == 0:
-            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                  % (epoch, num_epochs, i, len(dataloader),
-                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+#         netG.zero_grad()
+#         label.fill_(real_label)
+#         output = netD(fake).view(-1)
+#         errG = criterion(output, label)
+#         errG.backward()
+#         D_G_z2 = output.mean().item()
+#         optimizerG.step()
+#         if i % 50 == 0:
+#             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+#                   % (epoch, num_epochs, i, len(dataloader),
+#                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
-        G_losses.append(errG.item())
-        D_losses.append(errD.item())
-        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
-            with torch.no_grad():
-                fake = netG(fixed_noise).detach().cpu()
-            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+#         G_losses.append(errG.item())
+#         D_losses.append(errD.item())
+#         if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+#             with torch.no_grad():
+#                 fake = netG(fixed_noise).detach().cpu()
+#             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
-        iters += 1
+#         iters += 1
         
 
-fig = plt.figure(figsize=(8,8))
-plt.axis("off")
-ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+# fig = plt.figure(figsize=(8,8))
+# plt.axis("off")
+# ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
